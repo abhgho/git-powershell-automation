e562883685e0ffa3b7882f1095be15ebe08958cd
abhgho <abhik.ghosh84@gmail.com>
2021-10-31 00:17:47 +0530

    new script for create and add new repo to local

--- ConnectNew-GitRepo.ps1
+++ ConnectNew-GitRepo.ps1
@@ -1,2 +1,44 @@
-git init
-git branch -M main
+$fileContent = Get-Content -Path .\config
+$accessToken = ''
+$repoName = $args[0]
+
+foreach($line in $fileContent){
+    if($line.Contains('PERSONAL_ACCESS_TOKEN')){
+        $accessToken = $line.Substring(22)
+    }
+}
+
+$user = 'abhgho'
+$pass = $accessToken
+
+$pair = "$($user):$($pass)"
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+
+$basicAuthValue = "Basic $encodedCreds"
+
+$Headers = @{
+    Authorization = $basicAuthValue
+}
+$body = @{
+    name = $repoName
+    accept = 'application/vnd.github.v3+json'
+    private = $false
+}
+
+$bodyJson = ConvertTo-Json $body
+
+Write-Host "Posting Data: "$bodyJson
+
+$response = Invoke-WebRequest -Method 'POST' -Uri 'https://api.github.com/user/repos' -Headers $Headers -Body $bodyJson
+
+$responseJson = $response | ConvertFrom-Json
+
+Write-Host $responseJson.html_url" >>> Created!"
+
+$gitUrl = $responseJson.html_url
+
+
+git remote add origin "$gitUrl.git"
+
+Write-Host "Remoe Host Added"
